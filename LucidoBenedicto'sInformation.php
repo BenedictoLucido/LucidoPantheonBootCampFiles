eac4d09d5f547abdcded09e20001c51c9cf21ce3
Ben Lucido <84779677+xxlegendarybenxx@users.noreply.github.com>
2021-08-03 15:16:57 +0800

    Assignment Information about me

---
+++ LucidoBenedicto'sInformation.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lucido Benedicto Information</title>
+</head>
+<body>
+	<h1> HI! Welcome to my BIO </h1>
+	<?php 
+		echo "My Name is Benedicto E. Lucido Jr. <br>";
+		echo "I'm From Makati Pinagkaisahan <br>";
+		echo "I'm 19 years old<br>";
+		echo "My Favorite color is Blue <br>";
+		echo "My Favorite Food is Steak <br>";
+		echo "I'm Incoming Second Year College <br>";
+		echo "I'm Studying at The University of Makati <br>";
+	?>
+</body>
+</html>
